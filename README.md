37033b1bd554ce3fbf1c480a7abaaee70f976d43
Jurjen Sytsma <jurjen@collabomedia.nl>
2016-06-03 14:38:22 +0200

    Updated section 1.2

--- README.md
+++ README.md
@@ -31,10 +31,13 @@ more importantly; it's always better to avoid inline scripting/styling.
 **How to fix?**
 
 ```php
+remove_action( 'admin_print_styles', 'print_emoji_styles' );
 remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
 remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
 remove_action( 'wp_print_styles', 'print_emoji_styles' );
-remove_action( 'admin_print_styles', 'print_emoji_styles' );
+remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
 ```
 
 ### 1.3 Disable admin bar
--- functions.php
+++ functions.php
@@ -4,10 +4,13 @@
 add_filter( 'jpeg_quality', function( $arg ) { return 100; });
 
 // 1.2. Disable Emojicons
+remove_action( 'admin_print_styles', 'print_emoji_styles' );
 remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
 remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
 remove_action( 'wp_print_styles', 'print_emoji_styles' );
-remove_action( 'admin_print_styles', 'print_emoji_styles' );
+remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
 
 // 1.3. Disable Admin bar
 show_admin_bar(false);
